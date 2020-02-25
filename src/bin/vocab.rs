0456a55c0f6ec07d97ea4196f49b9366d1b508db
Daniel Mason <daniel@danielmason.com>
2020-02-25 06:05:59 +0000

    feat(bin): Better error handling

--- src/bin/vocab.rs
+++ src/bin/vocab.rs
@@ -38,8 +38,6 @@
 //!
 //! ToDo!
 
-use std::error::Error;
-
 use structopt::StructOpt;
 
 use vocab::{Translation, VocabStore, VocabStoreError};
@@ -66,17 +64,32 @@ enum Command {
 const SQLITE_FILE: &str = "vocab.sqlite";
 
 fn main() {
-    if let Err(e) = app() {
-        eprintln!("Something went wrong {:?}", e);
+    match app() {
+        Ok(_) => std::process::exit(0),
+        Err(VocabStoreError::AlreadyInitialised) => {
+            eprintln!("Already initialised");
+        }
+        Err(VocabStoreError::NotInitialised) => {
+            eprintln!("Not initialised, run: ");
+            eprintln!("    vocab --init");
+        }
+        Err(VocabStoreError::DuplicateEntry) => {
+            eprintln!("Already stored that translation");
+        }
+        Err(e) => eprintln!("Something went wrong {:?}", e),
     }
+    std::process::exit(1);
 }
 
-fn app() -> Result<(), Box<dyn Error>> {
+fn app() -> Result<(), VocabStoreError> {
     match VocabApp::from_args().subcommand.unwrap_or(Command::Multi) {
-        Command::Init => initialise_and_exit(),
+        Command::Init => {
+            VocabStore::init(SQLITE_FILE)?;
+            println!("Database initialised");
+        },
         Command::Add { local, foreign } => {
             let translation = Translation::new(&local, &foreign);
-            get_vocab_store().add(&translation)?;
+            VocabStore::from(SQLITE_FILE)?.add(&translation)?;
         }
         Command::Single => {
             println!("Commands::Single");
@@ -88,33 +101,4 @@ fn app() -> Result<(), Box<dyn Error>> {
         }
     };
     Ok(())
-}
-
-fn get_vocab_store() -> VocabStore {
-    match VocabStore::from(SQLITE_FILE) {
-        Ok(vs) => {
-            return vs;
-        }
-        Err(VocabStoreError::NotInitialised) => {
-            eprintln!("Not initialised, run: ");
-            eprintln!("    vocab --init");
-        }
-        Err(e) => eprintln!("Unknown error {:?}", e),
-    }
-    std::process::exit(1);
-}
-
-fn initialise_and_exit() {
-    match VocabStore::init(SQLITE_FILE) {
-        Ok(_) => {
-            println!("Database initialised");
-            std::process::exit(0)
-        }
-        Err(_e @ VocabStoreError::AlreadyInitialised) => {
-            eprintln!("Already initialised");
-            std::process::exit(0)
-        }
-        Err(e) => eprintln!("Could not init store {}", e),
-    }
-    std::process::exit(1);
 }
